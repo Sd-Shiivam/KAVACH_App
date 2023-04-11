d319f713d9dc05ed65e810c7c2769ad94446932a
Shivam-Singh <sd.shivam.00@gmail.com>
2023-04-11 09:19:20 +0530

    Base app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -110,20 +110,24 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(left: 1, top: 20, right: 1),
               child: CarouselSlider(
                 options: CarouselOptions(height: 200.0),
-                items: ["Shivam", 2, 3, 4, 5].map((i) {
+                items: [
+                  "assets/images/kvhlogo.png",
+                  "assets/images/msgspam.jpg",
+                  "assets/images/kvhlogo.png"
+                ].map((i) {
                   return Builder(
                     builder: (BuildContext context) {
                       return Container(
                           margin: EdgeInsets.symmetric(horizontal: 5.0),
                           decoration: BoxDecoration(
-                              color: Colors.amber,
+                              // color: Colors.amber,
+
                               borderRadius: BorderRadius.circular(20)),
                           child: Center(
-                            child: Text(
-                              'text $i',
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                          ));
+                              child: Image(
+                                  image: AssetImage(
+                            "$i",
+                          ))));
                     },
                   );
                 }).toList(),
